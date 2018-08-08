3029c56980208b0d1ede8881fa5da2a6e957abdb
Guillaume beuzeboc <beuzeboc@magazino.eu>
2018-08-08 15:48:55 +0200

    fix(installMosquitto): add libMosquittopp

--- installMosquitto.zsh
+++ installMosquitto.zsh
@@ -1,3 +1,3 @@
-sudo apt install -y mosquitto mosquitto-clients
+sudo apt install -y mosquitto mosquitto-clients libmosquittopp-dev 
 version=$(cmake --version | awk 'NR==1' | awk '{print $3}' | grep -e "..." -o)
 sudo cp cfg/FindMosquittopp.cmake /usr/share/cmake-${version}/Modules/
